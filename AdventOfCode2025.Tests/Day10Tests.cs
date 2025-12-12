848ecff0eb9d7357be8829798f27b69a33b36297
Christian Schabetsberger <christian.schabetsberger@finxit.at>
2025-12-12 14:51:51 +0100

    feat: day 10

---
+++ AdventOfCode2025.Tests/Day10Tests.cs
@@ -0,0 +1,40 @@
+﻿namespace AdventOfCode2025.Tests;
+
+public class Day10Tests
+{
+    [Test]
+    public async Task Part1()
+    {
+        // Arrange
+        var input = """
+                    [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+                    [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+                    [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+                    """;
+        var systemUnderTest = new Day10(input);
+
+        // Act
+        var result = await systemUnderTest.Solve_1();
+
+        // Assert
+        await Assert.That(result).IsEqualTo("7");
+    }
+
+    [Test, Skip("Part 2")]
+    public async Task Part2()
+    {
+        // Arrange
+        var input = """
+                    [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+                    [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+                    [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+                    """;
+        var systemUnderTest = new Day10(input);
+
+        // Act
+        var result = await systemUnderTest.Solve_2();
+
+        // Assert
+        await Assert.That(result).IsEqualTo("33");
+    }
+}
---
+++ AdventOfCode2025/Day10.cs
@@ -0,0 +1,92 @@
+﻿using System.Drawing;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2025;
+
+public class Day10 : BaseDay
+{
+    private readonly string _input;
+
+    public Day10()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public Day10(string input)
+    {
+        _input = input;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var regex = new Regex(@"\[([.#]+)\]( \((\d(?:,\d)*)\))+ \{(\d+(?:,\d+)*)\}", RegexOptions.Compiled);
+
+        var machines = new List<Machine>();
+
+        using var stringReader = new StringReader(_input);
+        while (stringReader.ReadLine() is { } line)
+        {
+            var match = regex.Match(line);
+            // .##. -> 0110
+            var targetIndicatorLights = match.Groups[1].Value.ToCharArray().Select(c => c == '#').ToArray();
+            var buttons = match.Groups[3].Captures.Select(c =>
+                c.Value.Split(",", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray()).ToList();
+            var machine = new Machine { TargetIndicatorLights = targetIndicatorLights, Buttons = buttons };
+            machines.Add(machine);
+        }
+
+        var result = machines.Select(machine => machine.GetLeastAmountOfButtonsPressed()).Sum();
+
+        return ValueTask.FromResult($"{result}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+
+    // https://stackoverflow.com/a/57058345
+    public static IEnumerable<T[]> Combinations<T>(IEnumerable<T> source)
+    {
+        if (null == source)
+            throw new ArgumentNullException(nameof(source));
+
+        T[] data = source.ToArray();
+
+        return Enumerable
+            .Range(0, 1 << (data.Length))
+            .Select(index => data
+                .Where((v, i) => (index & (1 << i)) != 0)
+                .ToArray());
+    }
+
+    class Machine
+    {
+        public required bool[] TargetIndicatorLights { get; set; }
+
+        public required List<int[]> Buttons { get; set; }
+
+        public int GetLeastAmountOfButtonsPressed()
+        {
+            var allCombinations = Combinations(Buttons).OrderBy(x => x.Length).ToList();
+            foreach (var combination in allCombinations)
+            {
+                var start = new bool[TargetIndicatorLights.Length];
+                foreach (var button in combination)
+                {
+                    foreach (var index in button)
+                    {
+                        start[index] = !start[index];
+                    }
+                }
+
+                if (Enumerable.SequenceEqual(TargetIndicatorLights, start))
+                {
+                    return combination.Length;
+                }
+            }
+
+            throw new Exception("No combination found");
+        }
+    }
+}
